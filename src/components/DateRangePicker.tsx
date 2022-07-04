64b30506f85bf5ebd948827d8eb4a9c88d9ed282
Hadassah Goldstein <hadassagoldstein@gmail.com>
2022-07-04 16:40:12 -0400

    fixed styling of range picker

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -62,7 +62,7 @@ export function DateRangePicker(props: DateRangePicker) {
     }
 
     return (
-        <flexboxLayout flexDirection="column" width={330} padding={10} borderColor="lightgrey" borderWidth={2} borderRadius="50px" backgroundColor="transparent">
+        <flexboxLayout flexDirection="column" width={330} padding={10} borderColor="lightgrey" borderWidth={2} backgroundColor="transparent">
             <flexboxLayout order={1} justifyContent="space-around">
                 <label fontSize='20px' text="<" onTap={() => setFirstDateOnMonth(addMonths(firstDateOnMonth, -1))} />
                 <label style={styles.nameOfMonth} text={format(firstDateOnMonth, 'MMMM yyyy')} />
