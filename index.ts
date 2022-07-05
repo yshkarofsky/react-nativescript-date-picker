bf1949024e05980f77525ca11ac04be39603dc28
Yocheved Shkarofsky <yshkarofsky@gmail.com>
2022-07-05 10:28:02 -0400

    increments version

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { DatePicker } from "./src/DatePicker";
+import { DateRangePicker } from "./src/DateRangePicker";
+import DateRangePickerModal from "./src/DateRangePickerModal";
+import DatePickerModal from "./src/ModalDatePicker"
+
+export {
+    DatePicker,
+    DateRangePicker,
+    DateRangePickerModal,
+    DatePickerModal,
+}
